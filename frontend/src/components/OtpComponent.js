aaea06e6bfe00145f9ffcfaab1dee03ab163b481
Piyush Jha <110280316+piyushcode56@users.noreply.github.com>
2025-04-04 22:02:51 +0530

    Update OtpComponent.js

--- frontend/src/components/OtpComponent.js
+++ frontend/src/components/OtpComponent.js
@@ -18,7 +18,7 @@ const OtpComponent = () => {
         // const formData = new FormData();
         try{
             // formData.append('otp', otp)
-            const url = 'https://campuscollab.onrender.com/user/verifyotp';
+            const url = 'https://campuscollab-backend.onrender.com/user/verifyotp';
             const response = await fetch(url, {
                 method:'POST',
                 headers:{
@@ -52,7 +52,7 @@ const OtpComponent = () => {
         e.preventDefault();
 
         try{
-            const url = 'https://campuscollab.onrender.com/user/resetpasswordotp';
+            const url = 'https://campuscollab-backend.onrender.com/user/resetpasswordotp';
             const response = await fetch(url, {
                 method:'POST',
                 headers:{
