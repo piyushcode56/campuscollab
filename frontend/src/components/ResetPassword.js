481c3faaa2687b15686fd95aa769d2e6bb3731ba
Piyush Jha <110280316+piyushcode56@users.noreply.github.com>
2025-04-04 22:03:12 +0530

    Update ResetPassword.js

--- frontend/src/components/ResetPassword.js
+++ frontend/src/components/ResetPassword.js
@@ -23,7 +23,7 @@ const ResetPassword = () => {
         e.preventDefault();
     
         try{
-            const url = 'https://campuscollab.onrender.com/user/resetpassword';
+            const url = 'https://campuscollab-backend.onrender.com/user/resetpassword';
             const response = await fetch(url, {
                 method:'POST',
                 headers:{
