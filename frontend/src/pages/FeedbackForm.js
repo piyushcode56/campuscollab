507586027f43660513c5c213f2448cfa21ec1251
Piyush Jha <110280316+piyushcode56@users.noreply.github.com>
2025-04-22 01:45:43 +0530

    Update FeedbackForm.js

--- frontend/src/pages/FeedbackForm.js
+++ frontend/src/pages/FeedbackForm.js
@@ -3,7 +3,7 @@ import React from 'react'
 const FeedbackForm = () => {
   return (
 
-    <div>
+    <div style={{textAlign:'center'}}>
         <p>Kindly click on link and fill the feedback form</p>
       <a href="https://forms.gle/tJ3FwHoTgdXNRd8S8">https://forms.gle/tJ3FwHoTgdXNRd8S8</a>
     </div>
