8867da26e5704ef281a7e6b8f119dab04f697ce3
Piyush Jha <110280316+piyushcode56@users.noreply.github.com>
2025-04-22 00:43:05 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -103,7 +103,7 @@ const App = () => {
         <Route element={<ProtectedRoute/>}>
         <Route path='/' element={<HomePage users={users}/>}/>
         <Route path='/myprojects' element={<MyProjects/>}/>
-        <Route path='/addprojects' element={<AddProject/>}/>
+        <Route path='/addprojects' element={<AddProject projects={projects}/>}/>
         <Route path='/favourites' element={<Favourites/>}/>
         <Route path='/campuscollab/search' element={<SearchUser users={users}/>}/>
         </Route>
