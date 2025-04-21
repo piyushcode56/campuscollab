5498c088dc3820c5ec8a59d4bff8fce3051fdf79
Piyush Jha <110280316+piyushcode56@users.noreply.github.com>
2025-04-21 14:34:19 +0530

    Update authValidation.js

--- backend/middlewares/authValidation.js
+++ backend/middlewares/authValidation.js
@@ -8,11 +8,7 @@ const signupValidation = (req, res, next) => {
         username:Joi.string().min(3).max(100).required(),
         email:Joi.string().min(3).max(100).email().required(),
         password:Joi.string().min(3).max(100).required(),
-<<<<<<< HEAD
-        
-=======
 
->>>>>>> 8b1939a6c9ac38500d05a15c6c357cedf771d53f
     })
 
     const {error} = schema.validate(req.body);
