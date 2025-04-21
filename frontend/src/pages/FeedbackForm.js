9d5e5375c4336cebcfeb9578687924e833cdb863
Piyush Jha <110280316+piyushcode56@users.noreply.github.com>
2025-04-22 00:56:41 +0530

    Create FeedbackForm.js

---
+++ frontend/src/pages/FeedbackForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FeedbackForm = () => {
+  return (
+
+    <div>
+        <p>Kindly click on link and fill the feedback form</p>
+      <a href="https://forms.gle/tJ3FwHoTgdXNRd8S8">https://forms.gle/tJ3FwHoTgdXNRd8S8</a>
+    </div>
+  )
+}
+
+export default FeedbackForm
